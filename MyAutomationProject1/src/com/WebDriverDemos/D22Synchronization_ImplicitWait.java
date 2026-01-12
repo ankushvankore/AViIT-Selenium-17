1ed2bd60adb947d4effecaf6663099e46ea9ade2
Ankush <ankushvankore@gmail.com>
2026-01-12 14:10:12 +0530

    Date: 12 Jan 2026

---
+++ MyAutomationProject1/src/com/WebDriverDemos/D22Synchronization_ImplicitWait.java
@@ -0,0 +1,29 @@
+package com.WebDriverDemos;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class D22Synchronization_ImplicitWait {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin123");
+        driver.findElement(By.className("oxd-button")).click();
+
+        if(driver.getCurrentUrl().contains("dashboard")){
+            System.out.println("Login succesful");
+            driver.findElement(By.className("bi-caret-down-fill")).click();
+            driver.findElement(By.linkText("Logout")).click();
+        }
+
+        driver.close();
+    }
+}
