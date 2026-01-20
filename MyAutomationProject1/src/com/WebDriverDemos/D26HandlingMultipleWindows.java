ca7e67857064e9642e675f7768ee911595ae6cb4
Ankush <ankushvankore@gmail.com>
2026-01-20 14:25:32 +0530

    Date: 20 Jan 2026

---
+++ MyAutomationProject1/src/com/WebDriverDemos/D26HandlingMultipleWindows.java
@@ -0,0 +1,53 @@
+package com.WebDriverDemos;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+import java.util.Set;
+
+public class D26HandlingMultipleWindows {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://www.naukri.com/");
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        js.executeScript("window.scrollBy(0, 400)", "");
+        //Thread.sleep(2000);
+        WebElement remote = driver.findElement(By.xpath("//span[text()=\"Remote\"]"));
+        js.executeScript("arguments[0].click()", remote);
+        System.out.println("Title: " + driver.getTitle());
+
+        Set<String>windows = driver.getWindowHandles();
+        System.out.println(windows);
+
+        Object[] winArray = windows.toArray();
+        String win1 = winArray[0].toString();       //id of 1st window
+        String win2 = winArray[1].toString();       //id of 2nd window
+
+        driver.switchTo().window(win2);
+        Thread.sleep(5000);
+        System.out.println("Title: " + driver.getTitle());
+
+        js.executeScript("window.scrollBy(0, 100)", "");
+        WebElement chkBox = driver.findElement(By.xpath("//*[@id=\"search-result-container\"]/div[1]/div[1]/div/div/div[2]/div[1]/div[2]/div[1]/label/i"));
+        js.executeScript("arguments[0].click()", chkBox);
+
+        Thread.sleep(2000);
+
+        driver.switchTo().window(win1);
+
+        driver.findElement(By.linkText("Login")).click();
+
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}
