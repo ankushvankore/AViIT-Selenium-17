a762695cd17c21d11847be46f5e85e07c6e82b0f
Ankush <ankushvankore@gmail.com>
2026-02-11 14:25:40 +0530

    Date: 11 Feb 2026

---
+++ MyAutomationProject1/src/com/TestNGDemos/D06DataProviderDemo.java
@@ -0,0 +1,57 @@
+package com.TestNGDemos;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.*;
+
+import java.time.Duration;
+
+public class D06DataProviderDemo {
+
+    WebDriver driver;
+    @Test (dataProvider = "getLoginData")
+    public void login(String un, String ps){
+        driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys(un);
+        driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys(ps);
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+    }
+
+    @DataProvider
+    public Object[][] getLoginData(){
+        Object[][]data = {
+                new Object[] {"admin", "admin123"},
+                new Object[] {"ila", "ila123"},
+                new Object[] {"shri", "shri123"},
+                new Object[] {"admin", "admin123"}
+        };
+
+        return  data;
+    }
+
+    @BeforeTest
+    public void setup(){
+        driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    }
+
+    @AfterTest
+    public void tearDown(){
+        driver.close();
+    }
+
+    @AfterMethod
+    public void logout(){
+        if(driver.getCurrentUrl().contains("dashboard")) {
+            driver.findElement(By.xpath("//i[@class='oxd-icon bi-caret-down-fill oxd-userdropdown-icon']")).click();
+            driver.findElement(By.linkText("Logout")).click();
+            System.out.println("Test case pass");
+        }
+        else {
+            System.out.println("Test case fail");
+        }
+    }
+}
